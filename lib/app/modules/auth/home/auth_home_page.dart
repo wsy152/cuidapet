3e4018fc10d53d1c415ff742bd3783b158f69b0a
Edvaldo <35712346+wsy152@users.noreply.github.com>
2022-04-21 16:36:10 -0300

    configuration size_screen_extension responsivity

--- lib/app/modules/auth/home/auth_home_page.dart
+++ lib/app/modules/auth/home/auth_home_page.dart
@@ -1,20 +1,25 @@
+import 'package:cuidapet_mobile/app/core/ui/extensions/size_screen_extensions.dart';
 import 'package:flutter/material.dart';
 
 class AuthHomePage extends StatefulWidget {
-
-  const AuthHomePage({ Key? key }) : super(key: key);
+  const AuthHomePage({Key? key}) : super(key: key);
 
   @override
   _AuthHomePageState createState() => _AuthHomePageState();
 }
 
 class _AuthHomePageState extends State<AuthHomePage> {
-
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text(''),),
-           body: Container(),
-       );
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset(
+          'assets/images/logo.png',
+          width: 150.w,
+          height: 200.h,
+          fit: BoxFit.contain,
+        ),
+      ),
+    );
   }
 }
