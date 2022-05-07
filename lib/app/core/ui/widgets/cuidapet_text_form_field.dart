5e492bf1e1a331ba16ad22f04895a25b950bd7a4
Edvaldo <35712346+wsy152@users.noreply.github.com>
2022-05-07 15:59:56 -0300

    configuration login page button

--- lib/app/core/ui/widgets/cuidapet_text_form_field.dart
+++ lib/app/core/ui/widgets/cuidapet_text_form_field.dart
@@ -4,28 +4,57 @@ class CuidapetTextFormField extends StatelessWidget {
   final String label;
   final TextEditingController? controller;
   final FormFieldValidator<String>? validator;
+  final bool obscureText;
+  final IconButton? suffixIcon;
+  final ValueNotifier<bool> _obscureTextNn;
 
-  const CuidapetTextFormField({
+  CuidapetTextFormField({
     Key? key,
     required this.label,
     this.controller,
     this.validator,
-  }) : super(key: key);
+    this.obscureText = false,
+    this.suffixIcon,
+  })  : _obscureTextNn = ValueNotifier<bool>(obscureText),
+        super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      controller: controller,
-      validator: validator,
-      decoration: InputDecoration(
-        labelText: label,
-        labelStyle:const TextStyle(fontSize: 15,color: Colors.black),
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(15)
-         
-        )
-      ),
-      
+    return ValueListenableBuilder<bool>(
+      valueListenable: _obscureTextNn,
+      builder: (_, obscureTextValue, child) {
+        return TextFormField(
+          controller: controller,
+          validator: validator,
+          obscureText: obscureTextValue,
+          decoration: InputDecoration(
+            labelText: label,
+            suffixIcon: obscureText
+                ? IconButton(
+                    onPressed: () {
+                      _obscureTextNn.value = !obscureTextValue;
+                    },
+                    icon: Icon(
+                      obscureTextValue ? Icons.lock : Icons.lock_open,
+                    ),
+                  )
+                : null,
+            labelStyle: const TextStyle(
+              fontSize: 15,
+              color: Colors.black,
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(15),
+              gapPadding: 5,
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(15),
+              gapPadding: 5,
+              borderSide: const BorderSide(color: Colors.grey),
+            ),
+          ),
+        );
+      },
     );
   }
 }
--- lib/app/modules/auth/login/widget/login_form.dart
+++ lib/app/modules/auth/login/widget/login_form.dart
@@ -1,3 +1,4 @@
+import 'package:cuidapet_mobile/app/core/ui/cuidapeticons_icons.dart';
 import 'package:cuidapet_mobile/app/core/ui/extensions/size_screen_extensions.dart';
 import 'package:cuidapet_mobile/app/core/ui/extensions/theme_extension.dart';
 import 'package:flutter/material.dart';
@@ -12,44 +13,67 @@ class LoginForm extends StatefulWidget {
 }
 
 class _LoginFormState extends State<LoginForm> {
+  final ctrlSenha = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-      const CuidapetTextFormField(label: 'Login'),
-      const CuidapetTextFormField(label: 'Senha'),
-       
+        SizedBox(
+          height: 10.h,
+        ),
+        CuidapetTextFormField(label: 'Login'),
+        SizedBox(
+          height: 20.h,
+        ),
+        CuidapetTextFormField(
+          label: 'Senha',
+          obscureText: true,
+          controller: ctrlSenha,
+          validator: (String? value){
+            if(value == null || value.isEmpty){
+              return 'Valor obrigatório';
+            }
+            return null;
+          },
+        ),
         const SizedBox(
           height: 20,
         ),
         Container(
-          padding: const EdgeInsets.all(10),
-          width: double.infinity,
-          height: 58,
-
-
-          child: ElevatedButton(onPressed: () {}, child:const Text('Entrar'))),
-
-          Row(
-            children: [
-              Expanded(
-                child: Divider(
-                  color: context.primaryColor,
-                  thickness: 1,
-                ),
+            padding: const EdgeInsets.all(10),
+            width: double.infinity,
+            height: 58,
+            child:
+                ElevatedButton(onPressed: () {}, child: const Text('Entrar'))),
+               const Icon(Cuidapeticons.facebook,color: Colors.blue, ),
+               const Icon(Cuidapeticons.google,color: Colors.red, ),
+               const Icon(Cuidapeticons.apple,color: Colors.cyan, ),
+        Row(
+          children: [
+            Expanded(
+              child: Divider(
+                color: context.primaryColor,
+                thickness: 1,
               ),
-               Padding(
-                padding:  const EdgeInsets.all(8.0),
-                child:  Text('OU',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 15.sp,color: context.primaryColor),),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'OU',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15.sp,
+                    color: context.primaryColor),
               ),
-               Expanded(
-                child: Divider(
-                  color: context.primaryColor,
-                  thickness: 1,
-                ),
-              )
-            ],
-          )
+            ),
+            Expanded(
+              child: Divider(
+                color: context.primaryColor,
+                thickness: 1,
+              ),
+            )
+          ],
+        )
       ],
     );
   }
